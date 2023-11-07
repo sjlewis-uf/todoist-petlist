dd58ac93ece5d3693a86afed66a7f0332a9b38c5
Sarah Lewis <sj.lewis@ufl.edu>
2023-11-06 21:36:57 -0500

    trying to deploy site

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -25,7 +25,6 @@ export const IndividualProject = ({project}) => {
             <span className="sidebar__project-name">{project.name}</span>
             <span className="sidebar__project-delete" data-testid="delete-project" 
              onClick={() => setShowConfirm(!showConfirm)}>
-                <X/>
              {showConfirm && (
                 <div className="project-delete-modal">
                     <div className="project-delete-modal__inner">
