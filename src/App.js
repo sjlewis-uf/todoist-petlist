00fbdbb046458087699adbc261511ddaabfad8d4
Sarah Lewis <sj.lewis@ufl.edu>
2023-11-09 21:28:41 -0500

    cleaning up files to resolve errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import PropTypes from 'prop-types';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 import { ProjectsProvider, SelectedProjectProvider } from './context';
