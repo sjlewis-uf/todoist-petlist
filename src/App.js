50686a26e6c73b581d7e9033ca8356403507cc2d
Sarah Lewis <sj.lewis@ufl.edu>
2023-12-05 21:41:52 -0500

    css edits

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 import { ProjectsProvider, SelectedProjectProvider } from './context';
+import "./App.scss";
 
 export const App = () => {
   return (
